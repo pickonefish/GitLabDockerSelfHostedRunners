566b316fae25dbe684ef2a4fc89e9a6cc4964127
PickOneFish <pickonefish@gmail.com>
2023-07-10 17:04:44 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,11 +4,8 @@ set -e
 # https://docs.gitlab.com/runner/configuration/advanced-configuration.html#the-executors
 
 gitlab-runner register \
-      --non-interactive \
       --url "$CI_SERVER_URL" \
       --token "$CI_SERVER_TOKEN" \
-      --executor "$RUNNER_EXECUTOR" \
-      --tag-list "$RUNNER_TAG_LIST" \
-      --name "$RUNNER_NAME"
+      --executor "$RUNNER_EXECUTOR"
 
 gitlab-runner --debug run;
